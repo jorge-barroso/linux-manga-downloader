316adcc3d15cd82bdf66b0fdebc8c8d209304db1
Jorge Barroso <jorge_barroso_11@hotmail.com>
2014-07-17 03:31:23 +0200

    Desktop file renamed and necessary corrections

--- uninstall.sh
+++ uninstall.sh
@@ -5,7 +5,7 @@ then
 		echo "Run this script as root user"
         else
 		rm /usr/bin/{4chan,animea,danboru,futahentai,ge-2,mangafox,mangareader,mangashare,mangatux,mangatux-fu,mcanime,pown,submanga,zerochan}
-                rm /usr/share/applications/Mangatux.desktop
+                rm /usr/share/applications/mangatux.desktop
                 rm /usr/share/icons/pen.png
                 rm -r /usr/share/licenses/mangatux/
                 rm -r /usr/share/mangatux/
@@ -15,14 +15,14 @@ else
 	if [ $HOME != /root ]
         then
 		sudo rm /usr/bin/{4chan,animea,danboru,futahentai,ge-2,mangafox,mangareader,mangashare,mangatux,mangatux-fu,mcanime,pown,submanga,zerochan}
-                sudo rm /usr/share/applications/Mangatux.desktop
+                sudo rm /usr/share/applications/mangatux.desktop
                 sudo rm /usr/share/icons/pen.png
                 sudo rm -r /usr/share/licenses/mangatux/
                 sudo rm -r /usr/share/mangatux/
                 sudo rm -r ~/.mangatux
 	else
 		rm /usr/bin/{4chan,animea,danboru,futahentai,ge-2,mangafox,mangareader,mangashare,mangatux,mangatux-fu,mcanime,pown,submanga,zerochan}
-                rm /usr/share/applications/Mangatux.desktop
+                rm /usr/share/applications/mangatux.desktop
                 rm /usr/share/icons/pen.png
                 rm -r /usr/share/licenses/mangatux/
                 rm -r /usr/share/mangatux/
